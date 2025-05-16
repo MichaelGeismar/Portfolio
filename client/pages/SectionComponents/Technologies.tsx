cceafb54f2da3ec6e97df92ae220b7557372de9a
MichaelGeismar <83302613+MichaelGeismar@users.noreply.github.com>
2025-05-16 11:55:54 -0600

    start styling cards and top banner (#1)

--- client/pages/SectionComponents/Technologies.tsx
+++ client/pages/SectionComponents/Technologies.tsx
@@ -2,6 +2,7 @@ import {
   familiarTechnologies,
   fluentTechnologies,
 } from "../data/technologiesInfo";
+import { techTalks } from "../data/aboutInfo";
 import CustomCard from "../customComponents/CustomCard";
 
 const Technologies = () => {
@@ -23,6 +24,16 @@ const Technologies = () => {
           <ul>
             {familiarTechnologies.map((tech, i) => (
               <li key={i}>{tech}</li>
+            ))}
+          </ul>
+        }
+      />
+      <CustomCard
+        title={"Tech Talks"}
+        content={
+          <ul>
+            {techTalks.map((techTalk, i) => (
+              <li key={i}>{techTalk}</li>
             ))}
           </ul>
         }
