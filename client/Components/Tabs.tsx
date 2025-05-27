8b66f6f90ad65c7d2854195b2ed498cb594d5cfd
MichaelGeismar <83302613+MichaelGeismar@users.noreply.github.com>
2025-05-27 10:16:21 -0600

    add styling for mobile (#5)

--- client/Components/Tabs.tsx
+++ client/Components/Tabs.tsx
@@ -14,13 +14,15 @@ const TabLayout = ({
       }
       defaultValue={sections[0].title}
     >
-      <div className="flex mx-8 mt-4 column min-h-[4rem] mb-4">
-        <div className="w-24 size-16 rounded-full bg-radial-[at_75%_25%] from-white to-green-900 to-75%"></div>
-        <p className="HeaderName text-2xl ms-4 content-center whitespace-nowrap">
-          {" "}
-          Michael Geismar{" "}
-        </p>
-        <Tabs.List className={"List"}>
+      <div className="flex flex-col md:flex-row mx-8 mt-4 min-h-fit md:min-h-[4rem] mb-4">
+        <div className="flex flex-row">
+          <div className="w-16 transition-transform duration-300 hover:scale-130 aspect-square self-center rounded-full bg-radial-[at_75%_25%] from-white to-green-900 to-75%"></div>
+          <p className="HeaderName text-2xl ms-4 content-center whitespace-nowrap">
+            {" "}
+            Michael Geismar{" "}
+          </p>
+        </div>
+        <Tabs.List className={"List flex flex-col md:flex-row"}>
           {sections.map((section: { title: string; id: number }) => (
             <Tabs.Tab className={"Tab"} value={section.title} key={section.id}>
               {section.title}
